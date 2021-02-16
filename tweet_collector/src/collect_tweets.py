da4566593a6480e94daca467a76762b6402998cb
Laurin Brinkmann <laurin.brinkmann@gmail.com>
2021-02-16 18:59:49 +0100

    tweet container writes to mongocontainer

--- tweet_collector/src/collect_tweets.py
+++ tweet_collector/src/collect_tweets.py
@@ -5,12 +5,16 @@ import tweepy
 import pymongo
 from dotenv import load_dotenv
 
+# Setting up Twitter connection
 load_dotenv()
-
 auth = tweepy.OAuthHandler(os.getenv('consumer_key'), os.getenv('consumer_secret'))
 auth.set_access_token(os.getenv('access_token'), os.getenv('access_token_secret'))
-
 api = tweepy.API(auth)
+
+# Setting up mongodb connection
+client = pymongo.MongoClient('mongodb://mongodb:27017')
+db = client.twitterdb
+
 cursor = tweepy.Cursor(
     api.search,
     q='marco rose',
@@ -18,5 +22,20 @@ cursor = tweepy.Cursor(
     lang='de'
 )
 
-for status in cursor.items(10):
-    print(status.user.screen_name, status.full_text)
+for status in cursor.items():
+    hashtags = status.entities['hashtags']
+    screen_name = status.user.screen_name
+    user_name = status.user.name
+    try:
+        tweet = status.retweeted_status.full_text
+    except AttributeError:  # Not a Retweet
+        tweet = status.full_text
+
+
+    db.rosenkrieg.insert_one({
+        'name': user_name,
+        'display_name': screen_name,
+        'tweet_text': tweet,
+        'hashtags': hashtags
+        })
+
