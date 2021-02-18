8602d3c0f00e4730f36003ff53170f81c6bb1903
Laurin Brinkmann <laurin.brinkmann@gmail.com>
2021-02-18 22:17:12 +0100

    etl removes duplicate tweets before writing to postgres

--- etl_sentiment/src/etl.py
+++ etl_sentiment/src/etl.py
@@ -1,19 +1,26 @@
 import pymongo
 import pandas as pd
+import logging
 from sqlalchemy import create_engine
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename='/log/etl.log', filemode='a'
+)
 
 con_string = 'postgresql://postgres:postgres@postgres:5432/twitterdb'
-
 client = pymongo.MongoClient('mongodb://mongodb:27017/')
-res = client.twitterdb.lwt2.find({}, {'display_name': 1, 'tweet_text': 1, '_id': 0} )
-
 sent = SentimentIntensityAnalyzer()
+
+
+logging.info('starting mongo query')
+res = client.twitterdb.lwt2.find({}, {'display_name': 1, 'tweet_text': 1, '_id': 0} )
 
 tweets = []
 for doc in res:
-
     sentiment = sent.polarity_scores(doc['tweet_text'])
     tweets.append([
             doc['display_name'],
@@ -21,12 +28,13 @@ for doc in res:
             sentiment['compound']
         ]
     )
+df_tweets = pd.DataFrame(tweets, columns=['screen_name', 'tweet', 'sentiment'])
+logging.info(f'fetched {len(df_tweets)} row from mongodb')
+
+df_tweets.drop_duplicates(['screen_name', 'tweet'], inplace=True)
+logging.info(f'after dropping duplicates, {(len(df_tweets))} lines will have to be written to db')
 
 
-df = pd.DataFrame(tweets, columns=['screen_name', 'tweet', 'sentiment'])
-print(df)
-
-
-engine = create_engine(con_string)
-df.to_sql(con=engine, name='sentimental_tweets', if_exists='replace')
+# engine = create_engine(con_string)
+# df.to_sql(con=engine, name='sentimental_tweets', if_exists='replace')
 
